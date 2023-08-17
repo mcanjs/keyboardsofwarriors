a56e96d215e5dfd8fba502ab2a2a0c2a19f4282f
mcanjs <mcanspin@gmail.com>
2023-08-17 23:36:42 +0300

    ecosystem changed

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,42 +7,39 @@
 module.exports = {
   apps: [
     {
-      name: 'prod', // pm2 start App name
+      name: 'app',
       script: 'dist/server.js',
-      exec_mode: 'cluster', // 'cluster' or 'fork'
-      instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
-      merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
+      exec_mode: 'cluster',
+      instance_var: 'INSTANCE_ID',
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      ignore_watch: ['node_modules', 'logs'],
+      max_memory_restart: '1G',
+      merge_logs: true,
+      output: './logs/app_access.log',
+      error: './logs/app_error.log',
       env: {
-        // environment variable
-        PORT: 3000,
+        PORT: 5000,
         NODE_ENV: 'production',
       },
     },
     {
-      name: 'dev', // pm2 start App name
-      script: 'ts-node', // ts-node
-      args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
-      exec_mode: 'cluster', // 'cluster' or 'fork'
-      instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
-      autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
-      ignore_watch: ['node_modules', 'logs'], // ignore files change
-      max_memory_restart: '1G', // restart if process use more than 1G memory
-      merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
-      output: './logs/access.log', // pm2 log file
-      error: './logs/error.log', // pm2 error log file
+      name: 'socket',
+      script: 'dist/server.js',
+      exec_mode: 'cluster',
+      instance_var: 'INSTANCE_ID',
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      ignore_watch: ['node_modules', 'logs'],
+      max_memory_restart: '1G',
+      merge_logs: true,
+      output: './logs/socket_access.log',
+      error: './logs/socket_error.log',
       env: {
-        // environment variable
-        PORT: 5000,
-        NODE_ENV: 'development',
+        PORT: 4000,
+        NODE_ENV: 'production',
       },
     },
   ],
@@ -50,7 +47,7 @@ module.exports = {
     production: {
       user: 'user',
       host: '0.0.0.0',
-      ref: 'origin/master',
+      ref: 'origin/main',
       repo: 'git@github.com:repo.git',
       path: 'dist/server.js',
       'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --only prod',
